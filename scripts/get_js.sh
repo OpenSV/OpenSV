b01d9c72ab85b05211032fabe566e6b57d886f8c
david thomson <jdavidthomson@hotmail.com>
2014-05-01 20:02:45 -0400

    first real commit

---
+++ scripts/get_js.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd ../OpenSV
+cd Players
+
+#clone jPlayer
+git clone https://github.com/happyworm/jPlayer.git
+
+#clone video.js
+git clone https://github.com/videojs/video.js.git
+
+

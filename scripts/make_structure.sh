14b6bf5085c41b142eaaee0bfb3a35958ac045b1
David Thomson <dt@appjacket.com>
2014-05-03 12:08:52 -0400

    Changed some lines to simplify the creation of folder.  Added a few lines to clone the OpenShift repo inside of Github/OpenSV.  Added a few lines to copy the Players to the OpenShift repo for predeployment.

--- scripts/make_structure.sh
+++ scripts/make_structure.sh
@@ -1,19 +1,31 @@
 #!/bin/sh
 
 mkdir ../OpenSV
-cd ../OpenSV
-mkdir Players
-cd ..
+mkdir ../OpenSV/Players
+mkdir ../OpenSV/OpenShift
+
 scripts/get_js_players.sh
+
+cd OpenSV/OpenShift
+git clone https://github.com/OpenSV/OpenShift.git
+cd ../..
+
 pwd
 ls -l
+
 cd OpenSV/Players/video.js
 npm install
 grunt
 cd ../../..
+
 pwd
+ls -l
+
 mkdir /var/www/opensv
 mkdir /var/www/opensv/js
+
 cp -R OpenSV/Players /var/www/opensv/js/
 cp html/samples/videojs.html /var/www/opensv
+
+cp -R OpenSV/Players OpenSV/OpenShift/OpenShift/app/js/
 

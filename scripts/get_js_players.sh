03603af2ad6217e509eec35ef71cce59a68bc353
David Thomson <dt@appjacket.com>
2014-05-04 18:36:21 -0400

    was missing the cloning of popcorn-js

--- scripts/get_js_players.sh
+++ scripts/get_js_players.sh
@@ -5,8 +5,12 @@ cd OpenSV/Players
 #clone jPlayer
 git clone https://github.com/happyworm/jPlayer.git
 
+#clone popcorn-js
+git clone https://github.com/mozilla/popcorn-js.git
+
 #clone video.js
 git clone https://github.com/videojs/video.js.git
+
 
 cd ../..
 
